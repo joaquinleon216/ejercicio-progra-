2245219650fee540ebd52cf79ae289dc48550e62
joaquinleon216 <joaquinleon216@gmail.com>
2023-03-28 18:06:52 -0500

    rectangulo

---
+++ Ejerciciosss/Ejerciciosss/rectangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejerciciosss
+{
+    class rectangulo : Shape
+    {
+        private float ba;
+        private float al;
+
+        public rectangulo(float ba, float al)
+        {
+            this.ba = ba
+            this.al = al;
+        }
+
+        public override float CalculateAreaRectangulo()
+        {
+            return ba * al;
+        }
+    }
+}
